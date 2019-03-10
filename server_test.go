0c6afee01a943713814e58dabede1bfa255d6102
Kévin Gomez <contact@kevingomez.fr>
2019-03-11 00:08:05 +0100

    Add a few tests

--- server_test.go
+++ server_test.go
@@ -2,6 +2,7 @@ package gostore
 
 import (
 	"bufio"
+	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 	"io/ioutil"
 	"log"
@@ -35,6 +36,22 @@ func TestExampleTestSuite(t *testing.T) {
 	suite.Run(t, new(serverTestSuite))
 }
 
+func sendRequest(require *require.Assertions, payload []byte) []byte {
+	conn, err := net.Dial("tcp", ":4224")
+	require.NoError(err, "could not connect to test server")
+	defer conn.Close()
+
+	conn.SetDeadline(time.Now().Add(1*time.Second))
+
+	_, err = conn.Write(payload)
+	require.NoError(err, "could not send test payload")
+
+	response, err := ioutil.ReadAll(bufio.NewReader(conn))
+	require.NoError(err, "could not read result")
+
+	return response
+}
+
 func (suite *serverTestSuite) TestItHandlesRequests() {
 	tt := []struct {
 		test    string
@@ -52,6 +69,11 @@ func (suite *serverTestSuite) TestItHandlesRequests() {
 			[]byte("10\nsome-value"),
 		},
 		{
+			"Data can be fetched",
+			[]byte("fetch unknown-key\n"),
+			[]byte("0\n"),
+		},
+		{
 			"Data can be deleted",
 			[]byte("del key\n"),
 			[]byte("OK"),
@@ -63,23 +85,28 @@ func (suite *serverTestSuite) TestItHandlesRequests() {
 		},
 	}
 
+	test := suite.Require()
+
 	for _, tc := range tt {
 		suite.Run(tc.test, func() {
-			require := suite.Require()
+			response := sendRequest(test, tc.payload)
 
-			conn, err := net.Dial("tcp", ":4224")
-			suite.NoError(err, "could not connect to test server")
-			defer conn.Close()
-
-			conn.SetDeadline(time.Now().Add(1*time.Second))
-
-			_, err = conn.Write(tc.payload)
-			require.NoError(err, "could not send test payload")
-
-			response, err := ioutil.ReadAll(bufio.NewReader(conn))
-			require.NoError(err, "could not read result")
-
-			require.Equal(tc.want, response)
+			test.Equal(tc.want, response)
 		})
 	}
+}
+
+func (suite *serverTestSuite) TestItHandlesExpiringKeys() {
+	test := suite.Require()
+
+	response := sendRequest(test, []byte("storex expiring-key 1s some-value\n"))
+	test.Equal([]byte("OK"), response)
+
+	response = sendRequest(test, []byte("fetch expiring-key\n"))
+	test.Equal([]byte("10\nsome-value"), response)
+
+	time.Sleep(time.Second)
+
+	response = sendRequest(test, []byte("fetch expiring-key\n"))
+	test.Equal([]byte("0\n"), response)
 }
